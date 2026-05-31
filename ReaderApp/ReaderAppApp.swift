6033d18c76d3e233cc3a9c5f6bf3949c3d028c27
Kevin Park <kevin.sangmin.park@gmail.com>
2026-05-30 23:05:07 -0400

    Initial Commit

---
+++ ReaderApp/ReaderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaderAppApp.swift
+//  ReaderApp
+//
+//  Created by Kevin Park on 5/30/26.
+//
+
+import SwiftUI
+
+@main
+struct ReaderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
